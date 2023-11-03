390fcaa966d650059ace65d6c481ccee7da7601b
LouiseMustafaeva <112573505+LouiseMustafaeva@users.noreply.github.com>
2023-11-03 16:56:13 +0500

    Initial Commit

---
+++ TheWords/TheWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheWordsApp.swift
+//  TheWords
+//
+//  Created by Louise on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TheWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
